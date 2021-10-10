fd7f58d8977c3fd2591bb1da56b41bf56517481f
Jakobwelna <jakob.welna@gmail.com>
2021-10-10 23:19:34 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/AVL_Tree_master.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Jakob/Documents/FH-Campus Wien/AVL_Tree-master/bintree.c" "C:/Users/Jakob/Documents/FH-Campus Wien/AVL_Tree-master/cmake-build-debug/CMakeFiles/AVL_Tree_master.dir/bintree.c.obj"
+  "C:/Users/Jakob/Documents/FH-Campus Wien/AVL_Tree-master/main.c" "C:/Users/Jakob/Documents/FH-Campus Wien/AVL_Tree-master/cmake-build-debug/CMakeFiles/AVL_Tree_master.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/AVL_Tree_master.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AVL_Tree_master.exe"
+  "AVL_Tree_master.exe.manifest"
+  "AVL_Tree_master.pdb"
+  "CMakeFiles/AVL_Tree_master.dir/bintree.c.obj"
+  "CMakeFiles/AVL_Tree_master.dir/main.c.obj"
+  "libAVL_Tree_master.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AVL_Tree_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
